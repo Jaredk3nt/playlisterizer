80a26b76dcb37fd1ec98eabb28c533635c91724f
Jared Jones <jkjones.dev@gmail.com>
2019-08-30 17:53:21 -0500

    Adding readme

--- src/components/Authorize.js
+++ src/components/Authorize.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const scopes = encodeURIComponent('user-read-private user-read-email');
-const clientId = encodeURIComponent('89117e931d6a40c59b08867327957a4e');
+const clientId = encodeURIComponent(process.env.REACT_APP_CLIENT_ID);
 const redirectUri = encodeURIComponent(process.env.REACT_APP_REDIRECT_URI);
 // Merge to a single auth URL
 const authUrl = `http://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`;
